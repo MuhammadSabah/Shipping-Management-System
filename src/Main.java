a80a4eb65b3c17ef257cd0a2a370a1e696b1833e
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2022-11-05 20:57:51 +0300

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import model.Employee;
+import view.ManagerView;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Shipping Management System");
+
     }
 }
